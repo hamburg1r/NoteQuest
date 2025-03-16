8c75cf505e46d53745dfb585777a3dbe3b8bef52
Hamburgir <shantnuprofid@gmail.com>
2025-03-16 21:59:06 +0530

    Added todo models

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,71 +1,37 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'todo.freezed.dart';
 part 'todo.g.dart';
 
 enum TodoState {
-	todo,
-	done,
-	doing,
-	next,
+  todo,
+  done,
+  doing,
+  next,
 }
 
 @unfreezed
 class TodoModel with _$TodoModel {
-	factory TodoModel({
-		required String id,
-		required String title,
-		@Default(3) int priority,
-		@Default([]) List<String> tag,
-		@Default(TodoState.todo) TodoState state,
-		DateTime? scheduledTime,
-		DateTime? dueTime,
-		List<String>? subTasks,
-	}) = _TodoModel;
+  factory TodoModel({
+    required String id,
+    required String title,
+    @Default(3) int priority,
+    @Default([]) List<String> tag,
+    @Default(TodoState.todo) TodoState state,
+    DateTime? scheduledTime,
+    DateTime? dueTime,
+    List<String>? subTasks,
+  }) = _TodoModel;
 
-	factory TodoModel.fromJson(Map<String, dynamic> json) => _$TodoModelFromJson(json);
+  factory TodoModel.fromJson(Map<String, dynamic> json) =>
+      _$TodoModelFromJson(json);
 }
 
-// class TodoModel {
-// 	final String id;
-// 	final String title;
-// 	final int priority;
-// 	final List<String> tag;
-// 	final TodoState state;
-// 	final DateTime? scheduledTime;
-// 	final DateTime? dueTime;
-// 	final List<String>? subTasks;
-//
-// 	TodoModel({
-// 		required this.title,
-// 		this.priority = 3,
-// 		this.tag = const [],
-// 		this.state = TodoState.todo,
-// 		this.scheduledTime,
-// 		this.dueTime,
-// 		this.subTasks,
-// 	}): id = Uuid().v4();
-//
-// 	TodoModel.fromJson(Map<String, dynamic> json):
-// 		id = json['id'] as String,
-// 		title = json['title'] as String,
-// 		priority = json['priority'] as int,
-// 		tag = json['tag'] as List<String>,
-// 		state = json['state'] as TodoState,
-// 		scheduledTime = json['scheduledTime']!=null? DateTime.fromMillisecondsSinceEpoch(json['scheduledTime'] as int): null,
-// 		dueTime = json['dueTime']!=null? DateTime.fromMillisecondsSinceEpoch(json['dueTime'] as int): null,
-// 		subTasks = (json['subTasks'] as List<dynamic>?)?.map((e) => e as String).toList();
-//
-// 	Map<Strin123g, dynamic> toJson() {
-// 		return {
-// 			'id': id,
-// 			'title': title,
-// 			'priority': priority,
-// 			'tag': tag,
-// 			'state': state.name,
-// 			'scheduledTime': scheduledTime?.millisecondsSinceEpoch,
-// 			'dueTime': dueTime?.millisecondsSinceEpoch,
-// 			'subTasks': subTasks,
-// 		};
-// 	}
-// }
+@riverpod
+class TodoList extends _$TodoList {
+  @override
+  Future<List<TodoModel>> build() async {
+    return <TodoModel>[];
+  }
+}
--- lib/models/todo.g.dart
+++ lib/models/todo.g.dart
@@ -44,3 +44,25 @@ const _$TodoStateEnumMap = {
   TodoState.doing: 'doing',
   TodoState.next: 'next',
 };
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$todoListHash() => r'48310cc6e3aa9f383776e9fc3c06ddfe3d555f65';
+
+/// See also [TodoList].
+@ProviderFor(TodoList)
+final todoListProvider =
+    AutoDisposeAsyncNotifierProvider<TodoList, List<TodoModel>>.internal(
+  TodoList.new,
+  name: r'todoListProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$todoListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TodoList = AutoDisposeAsyncNotifier<List<TodoModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
