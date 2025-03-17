3f729c3dbce366b124cc1d53bcf7fa8198f501d2
Hamburgir <shantnuprofid@gmail.com>
2025-03-17 14:55:20 +0530

    Counter done!

--- lib/widgets/counter.dart
+++ lib/widgets/counter.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Counter extends StatefulWidget {
-  Counter({this.controller, super.key});
+  const Counter({this.controller, super.key});
 
-  CounterController? controller;
+  final CounterController? controller;
 
   @override
   State<Counter> createState() => _CounterState();
@@ -15,23 +15,80 @@ class _CounterState extends State<Counter> {
   @override
   void initState() {
     super.initState();
-    controller = widget.controller ?? CounterController(count: 0);
+    controller = widget.controller ?? CounterController(initialValue: 0);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    BorderSide border =
+        BorderSide(color: ColorScheme.of(context).secondaryFixedDim);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        TextButton(onPressed: () {}, child: Icon(Icons.plus_one)),
-        TextButton(onPressed: () {}, child: Icon(Icons.plus_one)),
+        OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            maximumSize: Size(double.infinity, double.infinity),
+            shape: RoundedRectangleBorder(
+              side: border,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+              ),
+            ),
+          ),
+          onPressed: () {
+            setState(() {
+              controller.count--;
+            });
+          },
+          child: Icon(Icons.remove_circle),
+        ),
+        OutlinedButton(
+          onPressed: () {},
+          style: OutlinedButton.styleFrom(
+            maximumSize: Size(double.infinity, double.infinity),
+            foregroundColor: ColorScheme.of(context).secondaryFixedDim,
+            //backgroundColor: ColorScheme.of(context).secondaryFixedDim,
+            shape: RoundedRectangleBorder(
+              side: border,
+              //borderRadius: BorderRadius.only(
+              //  topLeft: Radius.circular(10),
+              //  bottomLeft: Radius.circular(10),
+              //),
+            ),
+          ),
+          child: Text(controller.count.toString()),
+        ),
+        OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            maximumSize: Size(double.infinity, double.infinity),
+            shape: RoundedRectangleBorder(
+              side: border,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+            ),
+          ),
+          onPressed: () {
+            setState(() {
+              controller.count++;
+            });
+          },
+          child: Icon(Icons.add_circle),
+        ),
       ],
     );
   }
 }
 
 class CounterController {
-  int count;
+  int _count;
+  set count(int value) {
+    _count = value < 0 ? 0 : value;
+  }
 
-  CounterController({this.count = 0});
+  int get count => _count;
+
+  CounterController({initialValue = 0}) : _count = initialValue;
 }
